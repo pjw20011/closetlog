e1ff4a24a250ff87641fe1fb32abaf6075bb0946
Jeongwon Park <pjw200116@gmail.com>
2025-05-06 23:58:52 +0900

    250506commit

--- src/main/java/com/jw/fashionreview/dto/StyleRecommendationDto.java
+++ src/main/java/com/jw/fashionreview/dto/StyleRecommendationDto.java
@@ -5,15 +5,21 @@ import java.util.List;
 
 public class StyleRecommendationDto {
     private Clothes top;
+    private Clothes outer; // 새로 추가
     private List<Clothes> recommendedBottoms;
 
-    public StyleRecommendationDto(Clothes top, List<Clothes> recommendedBottoms) {
+    public StyleRecommendationDto(Clothes top, Clothes outer, List<Clothes> recommendedBottoms) {
         this.top = top;
+        this.outer = outer;
         this.recommendedBottoms = recommendedBottoms;
     }
 
     public Clothes getTop() {
         return top;
+    }
+
+    public Clothes getOuter() {
+        return outer;
     }
 
     public List<Clothes> getRecommendedBottoms() {
@@ -24,4 +30,7 @@ public class StyleRecommendationDto {
         return top != null ? top.getColor() : null;
     }
 
+    public String getOuterColor() {
+        return outer != null ? outer.getColor() : null;
+    }
 }
